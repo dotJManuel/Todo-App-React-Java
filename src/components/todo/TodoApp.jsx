09439e275c62e2a4ee0463e0477005bc839b4264
dotJManuel <dotJManuel@outlook.com>
2025-01-06 23:11:51 -0600

    feat: add new components and router

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,10 +1,19 @@
 import { useState } from 'react'
+import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
 import './TodoApp.css'
 
 export default function TodoApp() {
   return (
       <div className="TodoApp">
-          <LoginComponent />
+          <BrowserRouter>
+            <Routes>
+                <Route path='/' element={ <LoginComponent /> }></Route>
+                <Route path='/login' element={ <LoginComponent /> }></Route>
+                <Route path='/welcome/:username' element={<WelcomeComponent /> }></Route>
+                <Route path='*' element={<ErrorComponent /> }></Route>
+                <Route path='/todos' element={<ListTodosComponent /> } />
+            </Routes>
+          </BrowserRouter>
       </div>
   )
 }
@@ -15,6 +24,7 @@ function LoginComponent() {
   const [password, setPassword] = useState('dummy')
   const [showSuccessMessage, setShowSuccessMessage] = useState(false)
   const [showErrorMessage, setShowErrorMessage] = useState(false)
+  const navigate = useNavigate();
 
   function handleUserNameChange(event) {
     setUserName(event.target.value)
@@ -28,6 +38,7 @@ function LoginComponent() {
     if(username==='in28minutes' && password==='dummy'){
         setShowSuccessMessage(true)
         setShowErrorMessage(false)
+        navigate(`/welcome/${username}`)
     } else {
         setShowSuccessMessage(false)
         setShowErrorMessage(true)
@@ -36,6 +47,7 @@ function LoginComponent() {
 
   return (
       <div className="Login">
+        <h1>Time to Login!</h1>
         {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
         {showErrorMessage && <div className="errorMessage">Authentication Failed.
                                                 Please check your credentials.</div>}
@@ -56,3 +68,72 @@ function LoginComponent() {
   )
 }
 
+function WelcomeComponent() {
+  const { username } = useParams()
+  return (
+      <div className="WelcomeComponent">
+         <h1>Welcome {username}</h1>
+          <div>
+            Manage your todos - <Link to="/todos">Go here</Link>
+          </div>
+      </div>
+  )
+}
+
+function ListTodosComponent() {
+
+  const today = new Date()
+
+  const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
+
+  const todos = [
+                  {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
+                  {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
+                  {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate},
+              ]
+
+
+  return (
+      <div className="ListTodosComponent">
+          <h1>Things You Want To Do!</h1>
+          <div>
+              <table>
+                  <thead>
+                          <tr>
+                              <td>ID</td>
+                              <td>Description</td>
+                              <td>Is Done?</td>
+                              <td>Target Date</td>
+                          </tr>
+                  </thead>
+                  <tbody>
+                  {
+                      todos.map(
+                          todo => (
+                              <tr key={todo.id}>
+                                  <td>{todo.id}</td>
+                                  <td>{todo.description}</td>
+                                  <td>{todo.done.toString()}</td>
+                                  <td>{todo.targetDate.toDateString()}</td>
+                              </tr>
+                          )
+                      )
+                  }
+                  </tbody>
+
+              </table>
+          </div>
+      </div>
+  )
+}
+
+function ErrorComponent() {
+  return (
+      <div className="ErrorComponent">
+          <h1>We are working really hard!</h1>
+          <div>
+              Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
+          </div>
+      </div>
+  )
+}
