6f6a2f30e999c0586639ab8a3f53aa11eb1d5816
dotJManuel <dotJManuel@outlook.com>
2025-01-07 22:58:22 -0600

    refactor: add components in each file

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,139 +1,29 @@
-import { useState } from 'react'
-import {BrowserRouter, Routes, Route, useNavigate, useParams, Link} from 'react-router-dom'
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import './TodoApp.css'
+import LogoutComponent from './LogoutComponent'
+import HeaderComponent from './HeaderComponent'
+import ListTodosComponent from './ListTodosComponent'
+import ErrorComponent from './ErrorComponent'
+import WelcomeComponent from './WelcomeComponent'
+import LoginComponent from './LoginComponent'
+import FooterComponent from './FooterComponent'
 
 export default function TodoApp() {
   return (
       <div className="TodoApp">
           <BrowserRouter>
+            <HeaderComponent />
             <Routes>
                 <Route path='/' element={ <LoginComponent /> }></Route>
                 <Route path='/login' element={ <LoginComponent /> }></Route>
                 <Route path='/welcome/:username' element={<WelcomeComponent /> }></Route>
-                <Route path='*' element={<ErrorComponent /> }></Route>
                 <Route path='/todos' element={<ListTodosComponent /> } />
+                <Route path='/logout' element={<LogoutComponent /> } />
+                <Route path='*' element={<ErrorComponent /> }></Route>
             </Routes>
+            <FooterComponent />
           </BrowserRouter>
-      </div>
-  )
-}
-
-function LoginComponent() {
-
-  const [username, setUserName] = useState('in28minutes')
-  const [password, setPassword] = useState('dummy')
-  const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-  const [showErrorMessage, setShowErrorMessage] = useState(false)
-  const navigate = useNavigate();
-
-  function handleUserNameChange(event) {
-    setUserName(event.target.value)
-  }
-
-  function handlePasswordChange(event) {
-    setPassword(event.target.value)
-  }
-
-  function handleSubmit() {
-    if(username==='in28minutes' && password==='dummy'){
-        setShowSuccessMessage(true)
-        setShowErrorMessage(false)
-        navigate(`/welcome/${username}`)
-    } else {
-        setShowSuccessMessage(false)
-        setShowErrorMessage(true)
-    }
-  }
-
-  return (
-      <div className="Login">
-        <h1>Time to Login!</h1>
-        {showSuccessMessage && <div className="successMessage">Authenticated Successfully</div>}
-        {showErrorMessage && <div className="errorMessage">Authentication Failed.
-                                                Please check your credentials.</div>}
-        <div className="LoginForm">
-            <div>
-                <label>User Name:</label>
-                <input type="text" name="username" value={username} onChange={handleUserNameChange}/>
-            </div>
-            <div>
-                <label>Password:</label>
-                <input type="password" name="password" value={password} onChange={handlePasswordChange}/>
-            </div>
-            <div>
-              <button type="button" name="login" onClick={handleSubmit}>login</button>
-            </div>
-        </div>
-      </div>
-  )
-}
-
-function WelcomeComponent() {
-  const { username } = useParams()
-  return (
-      <div className="WelcomeComponent">
-         <h1>Welcome {username}</h1>
-          <div>
-            Manage your todos - <Link to="/todos">Go here</Link>
-          </div>
-      </div>
-  )
-}
-
-function ListTodosComponent() {
-
-  const today = new Date()
-
-  const targetDate = new Date(today.getFullYear()+12, today.getMonth(), today.getDay())
-
-  const todos = [
-                  {id: 1, description: 'Learn AWS', done: false, targetDate:targetDate},
-                  {id: 2, description: 'Learn Full Stack Dev', done: false, targetDate:targetDate},
-                  {id: 3, description: 'Learn DevOps', done: false, targetDate:targetDate},
-              ]
-
-
-  return (
-      <div className="ListTodosComponent">
-          <h1>Things You Want To Do!</h1>
-          <div>
-              <table>
-                  <thead>
-                          <tr>
-                              <td>ID</td>
-                              <td>Description</td>
-                              <td>Is Done?</td>
-                              <td>Target Date</td>
-                          </tr>
-                  </thead>
-                  <tbody>
-                  {
-                      todos.map(
-                          todo => (
-                              <tr key={todo.id}>
-                                  <td>{todo.id}</td>
-                                  <td>{todo.description}</td>
-                                  <td>{todo.done.toString()}</td>
-                                  <td>{todo.targetDate.toDateString()}</td>
-                              </tr>
-                          )
-                      )
-                  }
-                  </tbody>
-
-              </table>
-          </div>
-      </div>
-  )
-}
-
-function ErrorComponent() {
-  return (
-      <div className="ErrorComponent">
-          <h1>We are working really hard!</h1>
-          <div>
-              Apologies for the 404. Reach out to our team at ABC-DEF-GHIJ.
-          </div>
       </div>
   )
 }
