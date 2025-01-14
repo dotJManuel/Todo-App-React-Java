bce24655ed417130b5ae0d2a2b6bdb64808b9f0f
dotJManuel <dotJManuel@outlook.com>
2025-01-13 23:42:24 -0600

    feat(hello-world): setting Todo REST API

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,8 +1,29 @@
 import {useParams, Link} from 'react-router-dom'
+import {useState} from 'react'
+import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService'
 
 function WelcomeComponent() {
 
     const {username } = useParams()
+
+    const [message, setMessage] = useState(null)
+
+    function callHelloWorldRestApi() {
+        retrieveHelloWorldPathVariable('JManuel')
+        .then( (response) => successfulResponse(response) )
+        .catch ( (error) => errorResponse(error) )
+        .finally ( () => console.log('cleanup') )
+    }
+
+    function successfulResponse(response) {
+        console.log(response)
+        // setMessage(response.data)
+        setMessage(response.data.message)
+    }
+
+    function errorResponse(error) {
+        console.log(error)
+    }
 
     return (
         <div className="WelcomeComponent">
@@ -10,6 +31,10 @@ function WelcomeComponent() {
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
             </div>
+            <div>
+                <button className="btn btn-success" onClick={callHelloWorldRestApi}>Call Hello world</button>
+            </div>
+            <div className="text-info">{message}</div>
         </div>
     )
 }
