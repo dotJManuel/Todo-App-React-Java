77075c687154556554f248f1883672f75ed57949
dotJManuel <dotJManuel@outlook.com>
2025-01-14 20:16:17 -0600

    refactor: format documents

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,29 +1,28 @@
 import {useParams, Link} from 'react-router-dom'
-import {useState} from 'react'
-import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService'
+// import {useState} from 'react'
+// import { retrieveHelloWorldPathVariable } from './api/HelloWorldApiService'
 
 function WelcomeComponent() {
 
     const {username } = useParams()
 
-    const [message, setMessage] = useState(null)
+    // const [message, setMessage] = useState(null)
 
-    function callHelloWorldRestApi() {
-        retrieveHelloWorldPathVariable('JManuel')
-        .then( (response) => successfulResponse(response) )
-        .catch ( (error) => errorResponse(error) )
-        .finally ( () => console.log('cleanup') )
-    }
+    // function callHelloWorldRestApi() {
+    //     retrieveHelloWorldPathVariable('JManuel')
+    //     .then( (response) => successfulResponse(response) )
+    //     .catch ( (error) => errorResponse(error) )
+    //     .finally ( () => console.log('cleanup') )
+    // }
 
-    function successfulResponse(response) {
-        console.log(response)
-        // setMessage(response.data)
-        setMessage(response.data.message)
-    }
+    // function successfulResponse(response) {
+    //     // setMessage(response.data)
+    //     setMessage(response.data.message)
+    // }
 
-    function errorResponse(error) {
-        console.log(error)
-    }
+    // function errorResponse(error) {
+    //     console.log(error)
+    // }
 
     return (
         <div className="WelcomeComponent">
@@ -31,10 +30,10 @@ function WelcomeComponent() {
             <div>
                 Manage your todos - <Link to="/todos">Go here</Link>
             </div>
-            <div>
+            {/* <div>
                 <button className="btn btn-success" onClick={callHelloWorldRestApi}>Call Hello world</button>
-            </div>
-            <div className="text-info">{message}</div>
+            </div> */}
+            {/* <div className="text-info">{message}</div> */}
         </div>
     )
 }
